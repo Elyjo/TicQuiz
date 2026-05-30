b5e99d4581a2bfd840fb38eb3b1351130bbd8440
Elyjo <j34744988@gmail.com>
2026-05-30 22:01:58 +0000

    data structure

---
+++ src/data/courses/d2a/l1/semester1/algorithmique.js
@@ -0,0 +1,35 @@
+export const algorithmique = {
+  id: "algorithmique",
+
+  title: "Algorithmique",
+
+  chapters: [
+    {
+      id: "chapter1",
+      title: "Données et opérations simples",
+      duration: "45 min",
+      questions: 20,
+    },
+
+    {
+      id: "chapter2",
+      title: "Structures conditionnelles",
+      duration: "40 min",
+      questions: 15,
+    },
+
+    {
+      id: "chapter3",
+      title: "Boucles",
+      duration: "50 min",
+      questions: 20,
+    },
+
+    {
+      id: "chapter4",
+      title: "Tableaux",
+      duration: "45 min",
+      questions: 20,
+    },
+  ],
+};
