c65a9db568ee56a24cce3157fa7cce6348b6e5fe
Elyjo <j34744988@gmail.com>
2026-06-13 02:07:35 +0000

    feat: adjusting the details

--- src/app/splash/page.jsx
+++ src/app/splash/page.jsx
@@ -9,7 +9,7 @@ export default function Splash() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.push("/login");
-    }, 2500);
+    }, 1500);
 
     return () => clearTimeout(timer);
   }, [router]);
