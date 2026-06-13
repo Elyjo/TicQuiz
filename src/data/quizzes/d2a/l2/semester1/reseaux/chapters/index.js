0f1d9ae562f9ba8b77a1d8b5a187fa827eddb397
Elyjo <j34744988@gmail.com>
2026-06-13 02:28:30 +0000

    feat: add cisco config chapter

--- src/data/quizzes/d2a/l2/semester1/reseaux/chapters/index.js
+++ src/data/quizzes/d2a/l2/semester1/reseaux/chapters/index.js
@@ -5,14 +5,16 @@ import { chapter2 } from "./chapitre-2";
 import { chapter3 } from "./chapitre-3";
 
 
+
 export const Quiz = {
   "chapter-1": chapter1,
   "chapter-2": chapter2,
   "chapter-3": chapter3,
+
 }
 
 export {
   chapter1,
   chapter2,
-  chapter3
+  chapter3,
 }
