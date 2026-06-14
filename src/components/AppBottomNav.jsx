7c7b4bedcc7847c3f7f1dd3ae1b90ebac0facab4
Elyjo <j34744988@gmail.com>
2026-06-14 13:05:03 +0000

    renaming js file and add new component

---
+++ src/components/AppBottomNav.jsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import BottomNav from "./BottomNav";
+
+export default function AppBottomNav() {
+  const pathname = usePathname();
+
+  const hiddenRoutes = [
+    "/login",
+    "/department",
+    "/levels",
+    "/subjects",
+    "/quiz",
+    "/result",
+  ];
+
+  const hideNav = hiddenRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (hideNav) return null;
+
+  return <BottomNav />;
+}
