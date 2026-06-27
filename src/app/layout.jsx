b3c31bacc85fbe0db89be1bd1db857f1bed189e4
Elyjo <j34744988@gmail.com>
2026-06-27 15:46:54 +0000

    feat: little rollback

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,8 +4,13 @@ import ResponsiveNav from "@/components/ResponsiveNav";
 import PwaInstallBanner from "@/components/PwaInstallBanner";
 
 export const metadata = {
+  title: "TicQuiz",
+  description: "Application de révision destinée aux étudiants",
   manifest: "/manifest.json",
-
+  icons: {
+    icon: "/tic-logo.png",
+    apple: "/tic-logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -13,11 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="min-h-full bg-[#020617] text-white">
         <PwaInstallBanner />
-        <Shell>
-          {children}
-        </Shell>
-
-        {/* NAVIGATION UNIFIÉE */}
+        <Shell>{children}</Shell>
         <ResponsiveNav />
       </body>
     </html>
