b345878454e786c2a1cef48024eb9533df9b5cdc
Elyjo <j34744988@gmail.com>
2026-06-28 18:49:35 +0000

    feat: remove pwabanner

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,6 @@
 import "./global.css";
 import Shell from "@/components/Shell";
 import ResponsiveNav from "@/components/ResponsiveNav";
-import PwaInstallBanner from "@/components/PwaInstallBanner";
 
 export const metadata = {
   title: "TicQuiz",
@@ -23,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="min-h-full bg-[#020617] text-white">
-        <PwaInstallBanner />
+        
         <Shell>{children}</Shell>
         <ResponsiveNav />
       </body>
